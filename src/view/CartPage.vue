c06a01d5df737253180d88369872c642a6d7ff6a
DESKTOP-EFK0K9M\victor <victorluizsantos49@gmail.com>
2023-11-04 20:28:31 -0300

    melhorado o carrinho

--- src/view/CartPage.vue
+++ src/view/CartPage.vue
@@ -3,7 +3,7 @@
 
 <template>
     <section class="container">
-        <div class="m-3 bg-light rounded-3">
+        <div class="mb-3 bg-light rounded-3">
             <div class="p-3">
                 <h5 class="p-2">10 Produtos Selecionados</h5>
                 <h5 class="p-2">Rua Bolívia, 342. Ponta Aguda, Blumenau-SC</h5>
@@ -12,35 +12,49 @@
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-3 card p-3">
-                <img src="../../public/cart.png" class="card-img-top w-50" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Nome Produto</h5>
-                    <p class="card-text">Descrição Produto</p>
-                    <p class="card-text">Valor Unitário</p>
+        <h3>Itens no Carrinho</h3>
+        <table class="table table-bordered table-hover table-striped table-responsive">
+            <thead>
+                <tr>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Quantidade</th>
+                    <th scope="col">Valor</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                    <td>
+                        <!--Nome do Produto-->
+                        <p class="d-inline-flex">
+                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#cartDetailsProduct">
+                                Banana Alabama Extension
+                            </button>
+                        </p>
+                        <!--Detalhes do Produto-->
+                        <div class="collapse" id="cartDetailsProduct">
+                            <div class="card card-body">
+                                Banana, pacoba ou pacova[1] é uma pseudobaga da bananeira, uma planta herbácea vivaz acaule
+                                da família Musaceae (género Musa - além do género Ensete, que produz as chamadas "falsas
+                                bananas"). São cultivadas em 130 países. Originárias do sudeste da Ásia são atualmente
+                                cultivadas em praticamente todas as regiões tropicais do planeta.
 
-                    <div class="d-flex align-items-baseline">
-                        <p class="card-text">Quantidade</p>
-                        <!--Botões Incrementar e Decrementar Quantidade-->
-                        <button class="btn btn-sm btn-outline-primary mx-1">
+                                <img src="../assets/products/banana.jpg" class="card-img-top w-75" alt="...">
+                            </div>
+                        </div>
+                        <!--Remover Produto-->
+                        <button class="btn btn-danger ms-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-plus" viewBox="0 0 16 16">
+                                class="bi bi-trash-fill" viewBox="0 0 16 16">
                                 <path
-                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                    d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                             </svg>
                         </button>
-                        <button class="btn btn-sm btn-outline-primary mx-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-dash" viewBox="0 0 16 16">
-                                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
-                            </svg>
-                        </button>
-                    </div>
-                    <p class="card-text">Valor Total</p>
-                </div>
-            </div>
-
-        </div>
+                    </td>
+                    <td>1</td>
+                    <td>R$ 45,50</td>
+                </tr>
+            </tbody>
+        </table>
     </section>
 </template>
